504a27640887d54afdca35d54a985ffd2fedec62
Nar Cuenca <narc.ph@gmail.com>
2017-07-05 11:37:33 +0800

    changed skill sim arrows and updated styling

--- my_builds.php
+++ my_builds.php
@@ -26,8 +26,8 @@ function display_content(){
 					<p>'.ucfirst($build_description).'</p>
 				</div>
 				<div class="card-action center-align">';
-					echo "<button class='waves-effect waves-teal btn blue-grey lighten-5 black-text btn-hover-scale'>Update</button>
-						<a href='#modal3' class='waves-effect waves-red btn blue-grey lighten-5 black-text btn-hover-scale' id='del".$id."' onclick='modal_pass(this.id)'>Delete</a>";
+					echo "<a href='build_update.php?build_id=$id' class='waves-effect waves-teal btn blue-grey lighten-5 black-text btn-hover-scale btn-margin'>Update</a>
+						<a href='#modal3' class='waves-effect waves-red btn blue-grey lighten-5 black-text btn-hover-scale btn-margin' id='del".$id."' onclick='modal_pass(this.id)'>Delete</a>";
 				echo '</div>
 			</div>
 		</a>';
@@ -41,8 +41,8 @@ function display_content(){
 			<span id="delIdReceiver" style="display: none"></span>
 		</div>
 		<div class="modal-footer center-align">
-			<button id="deleteBuildYes" class="btn red accent-1 btn-hover-scale">Yes</button>
-			<button class="modal-close btn teal accent-4 btn-hover-scale">No</button>
+			<button id="deleteBuildYes" class="btn red accent-1 btn-hover-scale btn-margin">Yes</button>
+			<button id="deleteBuildNo" class="modal-close btn teal accent-4 btn-hover-scale btn-margin">No</button>
 		</div>
 	</div>';
 };
--- skill_sim.php
+++ skill_sim.php
@@ -15,7 +15,7 @@ function display_content(){
 				<div class="input-field">
 					<form method="POST" action="" class="half-centered-form">';
 						multiDropdown('class');
-						echo '<input type="submit" name="skillSimSubmit" value="Select" class="btn btn-select">
+						echo '<input type="submit" name="skillSimSubmit" value="Select" class="btn btn-blue">
 					</form>
 				</div>
 			</div>
@@ -26,22 +26,22 @@ function display_content(){
 			$sql = "SELECT * FROM skills";
 			$result = mysqli_query($conn, $sql);
 			echo '<div class="row">
-				<div class="current-skill-tree col l12 m12 s12">
-					<h5 class="center-align">Current Skills</h5>
-					<form method="POST" action="skill_sim.php?class='.$class_input.'">';
+				<div class="bg z-depth-2 col l12 m12 s12">
+					<form method="POST" action="skill_sim.php?class='.$class_input.'">
+						<span>Unused Skill Points: <b><input type="text" id="sp_left" name="sp_left" value="49" style="width: 20px; border-bottom: none; margin: 0"></b></span>';
 						//Table for skills not tagged as Quest Skills
 						while($row = mysqli_fetch_assoc($result)){
 							extract($row);
 							if($class == $class_input && $quest_skill == 'No'){
-									echo'<div class="row">
+									echo'<div class="row row-no-margin">
 										<div class="'.$id.' col l5 m5 s12">
 											<img id="icon'.$skill_name.'" src="'.$icon.'"> <span>'.$skill_name.'</span>
 										</div>
-										<div class="skill-data'.$id.' col l4 m4 s6"><button id="add'.$skill_name.'" onclick="level(1,this.id)" type="button" class="add'.$id.'"><i class="material-icons">call_made</i></button>
-											<button id="min'.$skill_name.'" onclick="level(-1,this.id)" type="button" class="min'.$id.'"><i class="material-icons">call_received</i></button>
+										<div class="skill-data'.$id.' col l4 m4 s6"><button id="add'.$skill_name.'" onclick="level(1,this.id)" type="button" class="add'.$id.'"><img src="images/up-arrow.svg" alt="up" height="25"></button>
+											<button id="min'.$skill_name.'" onclick="level(-1,this.id)" type="button" class="min'.$id.'"><img src="images/down-arrow.svg" alt="down" height="25"></button>
 										</div>
 										<div class="skill-data'.$id.' col l3 m3 s6">
-												<input readonly type="text" id="level'.$skill_name.'" class="level'.$id.' current-skill-level" name="'.$skill_name.'" value="0" style="width: 20px; border-bottom: none; margin: 0">
+												<input readonly type="text" id="level'.$skill_name.'" class="level'.$id.' current-skill-level" name="'.$skill_name.'" value="0" style="width: 20px; border-bottom: none; margin: 0; height: 1em">
 												<span style="display:none" id="hidden'.$skill_name.'" class="hidden'.$id.'">0</span>
 												<span class="hide-on-small-only"> / </span>
 												<span id="max'.$skill_name.'" class="hide-on-small-only">'.$max_level.'</span><br>
@@ -51,21 +51,20 @@ function display_content(){
 												<span>Requires '.$unlock_requirements.' to unlock.</span>
 											</div>';
 										}
-									echo '</div>';
+									echo '</div><hr>';
 							};//non-quest skill closer
 							//Automatically sets value for skills tagged as Quest Skill to 1
 							if($class == $class_input && $quest_skill == 'Yes'){
-									echo '<div class="row">
-										<div class="'.$id.' col l5 m5 s12">
+									echo '<div class="row row-no-margin">
+										<div class="'.$id.' col l12 m12 s12">
 											<img id="icon'.$skill_name.'" src="'.$icon.'"> <span>'.$skill_name.' <b>[ Quest Skill ]</b></span>
 											<input hidden type="text" id="level'.$skill_name.'" name="'.$skill_name.'" value="1">
 										</div>
-									</div>';
+									</div><hr>';
 							};//quest skill closer
 						};//while loop closer
 							echo '<a href="#modal2"><button>Save</button></a>
 								<input type="submit" name="genRep" value="Generate Report">
-								Unused Skill Points: <input type="text" id="sp_left" name="sp_left" value="49" style="width: 20px; border-bottom: none; margin: 0">
 
 								<div id="modal2" class="modal">
 									<div class="modal-content">
